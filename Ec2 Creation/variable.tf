d94b1dcbd5765206b5784dff1ada108d92ffbe12
Sanjay T <71368949+sanjaythanigai@users.noreply.github.com>
2025-01-24 23:59:21 +0530

    Create variable.tf

---
+++ Ec2 Creation/variable.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region for the resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Availability zone for the subnet"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+}
